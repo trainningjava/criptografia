0ab99b95e457c1529b5f8803e4d57f3e9dca4f35
tati <tati@uol.com.br>
2024-03-18 23:22:25 -0300

    initial commit

---
+++ src/main/java/com/example/fernandakipper/FernandakipperApplication.java
@@ -0,0 +1,13 @@
+package com.example.fernandakipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandakipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FernandakipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fernandakipper/FernandakipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fernandakipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FernandakipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
